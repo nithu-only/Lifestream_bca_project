55dceb340aef10ba55bcfc5dd1a2ec5c1c34caf2
nithu-only <nithinneetu02@gmail.com>
2024-06-28 23:22:46 +0530

    First commit

---
+++ Lifestream/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<?php 
+include ('include/header.php');
+
+?>
+<html>
+
+
+	<head>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+		<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>LIFE STREAM</title>
+		<link rel="icon" type="image/png" href="image/logo.png">
+		
+		<body>
+			<div class="container-fluid red-background" style="margin-top: 700px;">
+				<div class="row">
+					<div class="col-md-12">
+						<h1 class="text-center"  style="color: white; font-weight: 700;padding: 10px 0 0 0;">Donate The Blood</h1>
+						<hr class="white-bar">
+						<p class="text-center pera-text" style="color:white;">
+							We are a group of exceptional programmers; our aim is to promote education. If you are a student, then contact us to secure your future. We deliver free international standard video lectures and content. We are also providing services in Web & Software Development.
+
+							We are a group of exceptional programmers; our aim is to promote education. If you are a student, then contact us to secure your future. We deliver free international standard video lectures and content. We are also providing services in Web & Software Development.
+						</p>
+						<a href="#" class="btn btn-default btn-lg text-center center-aligned">Become a Life Saver!</a>
+					</div>
+				</div>
+			</div>
+			
+		</body>
+		
+
+		</html>
+		<?php
+//include footer file
+include ('include/footer.php');
+ ?>
+	
